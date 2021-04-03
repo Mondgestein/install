87a24f0a4ec02ce6aabe51801e88a32cf45915e0
Ricardo Hanke <foreverwinter@hotmail.de>
2021-04-03 11:45:36 +0200

    Use internal copy routine

--- setup.c
+++ setup.c
@@ -11,6 +11,42 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dos.h>
+#include <fcntl.h>
+#include <direct.h>
+
+static char copy_buffer[4096];
+
+
+
+unsigned CopyFile(const char *src_file, const char *dst_file)
+{
+    unsigned bytes_read;
+    unsigned bytes_written;
+    unsigned result;
+    int src_handle;
+    int dst_handle;
+
+    if ((result = _dos_open(src_file, O_RDONLY, &src_handle)) == 0)
+    {
+        if ((result = _dos_creat(dst_file, _A_NORMAL, &dst_handle)) == 0)
+        {
+            while ((result = _dos_read(src_handle, &copy_buffer, sizeof(copy_buffer), &bytes_read)) == 0 && bytes_read > 0)
+            {
+                if ((result = _dos_write(dst_handle, copy_buffer, bytes_read, &bytes_written)) != 0)
+                {
+                    break;
+                }
+            }
+
+            _dos_close(dst_handle);
+        }
+
+        _dos_close(src_handle);
+    }
+
+    return result;
+}
 
 
 
@@ -77,9 +113,9 @@ int create_config_file(const char *filename, const char *country, const char *sy
 
     if (fp != NULL)
     {
-        fprintf(fp, "DEVICE=%sSETVER.SYS\n", sysroot);
+        fprintf(fp, "DEVICE=%s\\SETVER.SYS\n", sysroot);
         fprintf(fp, "DOS=HIGH\n");
-        fprintf(fp, "COUNTRY=%s,437,%sCOUNTRY.SYS\n", country, sysroot);
+        fprintf(fp, "COUNTRY=%s,437,%s\\COUNTRY.SYS\n", country, sysroot);
         fprintf(fp, "FILES=30\n");
         fclose(fp);
     }
@@ -120,6 +156,10 @@ int main(int argc, char *argv[])
     char source[_MAX_PATH];
     char dstdrv[_MAX_PATH];
     char buffer[_MAX_PATH];
+    char src_buffer[_MAX_PATH];
+    char dst_buffer[_MAX_PATH];
+    struct find_t finfo;
+    unsigned result;
 
 
     if (prompt_to_continue() == 0)
@@ -137,19 +177,43 @@ int main(int argc, char *argv[])
     _splitpath(argv[0], source, NULL, NULL, NULL);
     _splitpath(argv[1], dstdrv, NULL, NULL, NULL);
 
-    strcat(source, "\\*.*");
-
     if (spawnl(P_WAIT, "FORMAT.COM", "FORMAT.COM", dstdrv, "/S", NULL) == -1)
     {
         printf("\n\n\nCannot execute FORMAT.COM\n");
         return 0;
     }
 
-    if (spawnl(P_WAIT, "XCOPY.EXE", "XCOPY.EXE", source, argv[1], NULL) == -1)
+    if (mkdir(argv[1]) != 0)
     {
-        printf("\n\n\nCannot execute XCOPY.EXE\n");
+        printf("\n\n\nCannot create target directory\n");
         return 0;
     }
+
+    strcpy(src_buffer, source);
+    strcat(src_buffer, "\\*.*");
+
+    printf("\n\n");
+
+    for (result = _dos_findfirst(src_buffer, _A_NORMAL & _A_RDONLY & _A_ARCH, &finfo); result == 0; result = _dos_findnext(&finfo))
+    {
+        strcpy(src_buffer, source);
+        strcat(src_buffer,"\\");
+        strcat(src_buffer, finfo.name);
+
+        strcpy(dst_buffer, argv[1]);
+        strcat(dst_buffer, "\\");
+        strcat(dst_buffer, finfo.name);
+
+        printf("%s\n", finfo.name);
+
+        if (CopyFile(src_buffer, dst_buffer) != 0)
+        {
+            printf("\n\n\nCannot copy %s\n", finfo.name);
+            return 0;
+        }
+    }
+
+    _dos_findclose(&finfo);
 
     strcpy(buffer, dstdrv);
     strcat(buffer, "\\CONFIG.SYS");
